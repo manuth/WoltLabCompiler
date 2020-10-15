e03f9c521063c7aef2cf2a749930d524f7525fac
Manuel Thalmann <m@nuth.ch>
2020-10-15 18:04:54 +0200

    Refactor the `CronJobInstructionCompiler` tests

--- src/tests/Compilation/Instructions/CronJobInstructionCompiler.test.ts
+++ src/tests/Compilation/Instructions/CronJobInstructionCompiler.test.ts
@@ -1,51 +1,27 @@
-import { doesNotReject, ok } from "assert";
-import { TempDirectory } from "@manuth/temp-files";
-import { pathExists } from "fs-extra";
 import { CronJobInstructionCompiler } from "../../../Compilation/PackageSystem/Instructions/CronJobInstructionCompiler";
-import { ACPOptionInstruction } from "../../../PackageSystem/Instructions/Options/ACPOptionInstruction";
 import { CronJobInstruction } from "../../../PackageSystem/Instructions/Tasks/CronJobInstruction";
-import { Package } from "../../../PackageSystem/Package";
 import { TimePeriod } from "../../../Tasks/TimePeriod";
+import { CompilerTester } from "../TestComponents/Testers/CompilerTester";
+import { InstructionCompilerTestRunner } from "../TestComponents/TestRunners/InstructionCompilerTestRunner";
 
 /**
  * Registers tests for the `CronJobInstructionCompiler` class.
  */
 export function CronJobInstructionCompilerTests(): void
 {
-    suite(
-        "CronJobInstructionCompiler",
-        () =>
+    new class extends InstructionCompilerTestRunner<CompilerTester<CronJobInstructionCompiler>, CronJobInstructionCompiler>
+    {
+        /**
+         * @inheritdoc
+         *
+         * @returns
+         * The new compiler-tester instance.
+         */
+        protected CreateTester(): CompilerTester<CronJobInstructionCompiler>
         {
-            let tempDir: TempDirectory;
-            let fileName: string;
-            let compiler: CronJobInstructionCompiler;
-            let instruction: CronJobInstruction;
-
-            suiteSetup(
-                () =>
-                {
-                    tempDir = new TempDirectory();
-                    let $package: Package = new Package(
-                        {
-                            Identifier: "foo",
-                            DisplayName: {},
-                            InstallSet: {
-                                Instructions: [
-                                    new ACPOptionInstruction(
-                                        {
-                                            FileName: null,
-                                            Nodes: [
-                                                {
-                                                    ID: "foo",
-                                                    Name: "this-is-a-test"
-                                                }
-                                            ]
-                                        })
-                                ]
-                            }
-                        });
-
-                    instruction = new CronJobInstruction(
+            return new CompilerTester(
+                new CronJobInstructionCompiler(
+                    new CronJobInstruction(
                         {
                             FileName: "cronJobs.xml",
                             CronJobs: [
@@ -55,37 +31,7 @@ export function CronJobInstructionCompilerTests(): void
                                     Period: TimePeriod.Monthly
                                 }
                             ]
-                        });
-
-                    $package.InstallSet.push(instruction);
-                    compiler = new CronJobInstructionCompiler(instruction);
-                    compiler.DestinationPath = tempDir.FullName;
-                    fileName = compiler.DestinationFileName;
-                });
-
-            suiteTeardown(
-                () =>
-                {
-                    tempDir.Dispose();
-                });
-
-            suite(
-                "Compile",
-                () =>
-                {
-                    test(
-                        "Checking whether the instruction can be compiled without an error…",
-                        async () =>
-                        {
-                            await doesNotReject(async () => compiler.Execute());
-                        });
-
-                    test(
-                        "Checking whether the expected file exists…",
-                        async () =>
-                        {
-                            ok(await pathExists(fileName));
-                        });
-                });
-        });
+                        })));
+        }
+    }("CronJobInstructionCompiler").Register();
 }
