31b4c9532e0d93053839d151f89ce99886c356ac
Manuel Thalmann <m@nuth.ch>
2020-10-15 17:30:03 +0200

    Refactor tests for the `ACPOptionINstructionCompiler` class

--- src/tests/Compilation/Instructions/ACPOptionInstructionCompiler.test.ts
+++ src/tests/Compilation/Instructions/ACPOptionInstructionCompiler.test.ts
@@ -1,49 +1,39 @@
 import { ok } from "assert";
-import { TempDirectory } from "@manuth/temp-files";
 import { pathExists, readdir } from "fs-extra";
+import { join } from "upath";
 import { ACPOptionInstructionCompiler } from "../../../Compilation/PackageSystem/Instructions/ACPOptionInstructionCompiler";
 import { ILocalization } from "../../../Globalization/ILocalization";
 import { ACPOptionInstruction } from "../../../PackageSystem/Instructions/Options/ACPOptionInstruction";
 import { Package } from "../../../PackageSystem/Package";
+import { CompilerTester } from "../TestComponents/Testers/CompilerTester";
+import { CompilerTestRunner } from "../TestComponents/TestRunners/CompilerTestRunner";
 
 /**
  * Registers tests for the `ACPOptionInstructionCompiler` class.
  */
 export function ACPOptionInstructionCompilerTests(): void
 {
-    suite(
-        "ACPOptionInstructionCompiler",
-        () =>
+    new class extends CompilerTestRunner<CompilerTester<ACPOptionInstructionCompiler>, ACPOptionInstructionCompiler>
+    {
+        /**
+         * @inheritdoc
+         *
+         * @returns
+         * The new compiler-tester instance.
+         */
+        protected CreateTester(): CompilerTester<ACPOptionInstructionCompiler>
         {
-            let fileName: string;
-            let translationDir: string;
-            let tempDir: TempDirectory;
-            let compiler: ACPOptionInstructionCompiler;
-            let locales: string[];
+            let displayName: ILocalization = {};
+            let locales = ["en", "de", "it", "fr"];
 
-            suiteSetup(
-                () =>
-                {
-                    let displayName: ILocalization = {};
+            for (let locale of locales)
+            {
+                displayName[locale] = "test";
+            }
 
-                    let $package: Package = new Package(
-                        {
-                            Identifier: "foo",
-                            DisplayName: {},
-                            InstallSet: {
-                                Instructions: []
-                            }
-                        });
-
-                    tempDir = new TempDirectory();
-                    locales = ["en", "de", "it", "fr"];
-
-                    for (let locale of locales)
-                    {
-                        displayName[locale] = "test";
-                    }
-
-                    let instruction: ACPOptionInstruction = new ACPOptionInstruction(
+            let tester = new CompilerTester(
+                new ACPOptionInstructionCompiler(
+                    new ACPOptionInstruction(
                         {
                             FileName: "options.xml",
                             Nodes: [
@@ -54,46 +44,47 @@ export function ACPOptionInstructionCompilerTests(): void
                                     }
                                 }
                             ]
-                        });
+                        })));
 
-                    $package.InstallSet.push(instruction);
-                    compiler = new ACPOptionInstructionCompiler(instruction);
-                    compiler.DestinationPath = tempDir.FullName;
-                    fileName = compiler.DestinationFileName;
-                    translationDir = tempDir.MakePath(instruction.DestinationRoot, instruction.TranslationDirectory);
-                });
-
-            suiteTeardown(
-                () =>
+            new Package(
                 {
-                    tempDir.Dispose();
-                });
+                    Identifier: "foo",
+                    DisplayName: {},
+                    InstallSet: {
+                        Instructions: []
+                    }
+                }).InstallSet.push(tester.Compiler.Item);
 
-            suite(
-                "Compile",
-                () =>
+            return tester;
+        }
+
+        /**
+         * @inheritdoc
+         */
+        protected ExecuteTests(): void
+        {
+            super.ExecuteTests();
+
+            test(
+                "Checking whether the options-file exists…",
+                async () =>
                 {
-                    test(
-                        "Checking whether the compiler can be executed…",
-                        async () =>
-                        {
-                            await compiler.Execute();
-                        });
-
-                    test(
-                        "Checking whether the option-file exists…",
-                        async () =>
-                        {
-                            ok(await pathExists(fileName));
-                        });
-
-                    test(
-                        "Checking whether the language-files exist…",
-                        async () =>
-                        {
-                            let files: string[] = await readdir(translationDir);
-                            ok(locales.every((locale: string) => files.includes(`${locale}.xml`)));
-                        });
+                    ok(await pathExists(this.Compiler.DestinationFileName));
                 });
-        });
+
+            test(
+                "Checking whether the language-files exist…",
+                async () =>
+                {
+                    let files = await readdir(
+                        join(
+                            this.Compiler.DestinationPath,
+                            this.Compiler.Item.DestinationRoot,
+                            this.Compiler.Item.TranslationDirectory));
+
+                    let locales = Object.keys(this.Compiler.Item.GetMessages());
+                    ok(locales.every((locale) => files.includes(`${locale}.xml`)));
+                });
+        }
+    }("ACPOptionInstructionCompiler").Register();
 }
