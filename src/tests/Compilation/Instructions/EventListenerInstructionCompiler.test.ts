a8aa3ddd12de3e8ec4cadb3d0d0ac28d70628d3f
Manuel Thalmann <m@nuth.ch>
2020-10-15 18:18:08 +0200

    Refactor the `EventListenerInstructionCompiler` tests

--- src/tests/Compilation/Instructions/EventListenerInstructionCompiler.test.ts
+++ src/tests/Compilation/Instructions/EventListenerInstructionCompiler.test.ts
@@ -1,59 +1,30 @@
-import { doesNotReject, ok } from "assert";
-import { TempDirectory } from "@manuth/temp-files";
-import { pathExists } from "fs-extra";
 import { EventListenerInstructionCompiler } from "../../../Compilation/PackageSystem/Instructions/EventListenerInstructionCompiler";
 import { EventListenerInstruction } from "../../../PackageSystem/Instructions/Events/EventListenerInstruction";
-import { Package } from "../../../PackageSystem/Package";
+import { CompilerTester } from "../TestComponents/Testers/CompilerTester";
+import { InstructionCompilerTestRunner } from "../TestComponents/TestRunners/InstructionCompilerTestRunner";
 
 /**
  * Registers tests for the `EventListenerInstructionCompiler` class.
  */
 export function EventListenerInstructionCompilerTests(): void
 {
-    suite(
-        "EventListenerInstructionCompiler",
-        () =>
+    new class extends InstructionCompilerTestRunner<CompilerTester<EventListenerInstructionCompiler>, EventListenerInstructionCompiler>
+    {
+        /**
+         * @inheritdoc
+         *
+         * @returns
+         * The new compiler-tester instance.
+         */
+        protected CreateTester(): CompilerTester<EventListenerInstructionCompiler>
         {
-            let tempDir: TempDirectory;
-            let fileName: string;
-            let compiler: EventListenerInstructionCompiler;
-
-            suiteSetup(
-                () =>
-                {
-                    let $package: Package = new Package(
-                        {
-                            Identifier: "test",
-                            DisplayName: {},
-                            InstallSet: {
-                                Instructions: []
-                            }
-                        });
-
-                    let instruction: EventListenerInstruction = new EventListenerInstruction(
+            return new CompilerTester(
+                new EventListenerInstructionCompiler(
+                    new EventListenerInstruction(
                         {
                             FileName: "eventListeners.xml",
                             Listeners: []
-                        });
-
-                    $package.InstallSet.push(instruction);
-                    compiler = new EventListenerInstructionCompiler(instruction);
-                    compiler.DestinationPath = tempDir.FullName;
-                    fileName = compiler.DestinationFileName;
-                });
-
-            test(
-                "Checking whether the compiler can be executed…",
-                async () =>
-                {
-                    await doesNotReject(async () => compiler.Execute());
-                });
-
-            test(
-                "Checking whether the compiled file exists…",
-                async () =>
-                {
-                    ok(await pathExists(fileName));
-                });
-        });
+                        })));
+        }
+    }("EventListenerInstructionCompiler").Register();
 }
