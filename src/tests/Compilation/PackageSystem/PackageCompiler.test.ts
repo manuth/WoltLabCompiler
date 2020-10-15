937a6543d659140014a42af9a54cada92209a8de
Manuel Thalmann <m@nuth.ch>
2020-10-16 00:27:32 +0200

    Refactor the `PackageCompiler` tests

--- src/tests/Compilation/PackageSystem/PackageCompiler.test.ts
+++ src/tests/Compilation/PackageSystem/PackageCompiler.test.ts
@@ -1,78 +1,84 @@
 import { ok } from "assert";
-import { TempDirectory, TempFile } from "@manuth/temp-files";
-import { pathExists } from "fs-extra";
-import { extract } from "tar";
+import { TempDirectory } from "@manuth/temp-files";
+import { extract, list } from "tar";
 import { PackageCompiler } from "../../../Compilation/PackageSystem/PackageCompiler";
 import { Package } from "../../../PackageSystem/Package";
+import { CompilerTester } from "../TestComponents/Testers/CompilerTester";
+import { CompilerTestRunner } from "../TestComponents/TestRunners/CompilerTestRunner";
 
 /**
  * Registers tests for the `PackageCompiler` class.
  */
 export function PackageCompilerTests(): void
 {
-    suite(
-        "PackageCompiler",
-        () =>
+    let tempDir: TempDirectory;
+
+    new class extends CompilerTestRunner<CompilerTester<PackageCompiler>, PackageCompiler>
+    {
+        /**
+         * @inheritdoc
+         *
+         * @returns
+         * The new compiler-tester instance.
+         */
+        protected CreateTester(): CompilerTester<PackageCompiler>
         {
-            let archive: TempFile;
-            let archiveDir: TempDirectory;
-            let compiler: PackageCompiler;
-            let extensionPackage: Package;
-
-            suiteSetup(
-                () =>
-                {
-                    archive = new TempFile();
-                    archiveDir = new TempDirectory();
-
-                    extensionPackage = new Package(
+            return new CompilerTester(
+                new PackageCompiler(
+                    new Package(
                         {
                             Identifier: "com.example.mypackage",
                             DisplayName: {},
                             InstallSet: {
                                 Instructions: []
                             }
-                        });
+                        })));
+        }
 
-                    compiler = new PackageCompiler(extensionPackage);
-                    compiler.DestinationPath = archive.FullName;
-                });
+        /**
+         * @inheritdoc
+         */
+        protected async SuiteSetup(): Promise<void>
+        {
+            tempDir = new TempDirectory();
+            await super.SuiteSetup();
+        }
 
-            suiteTeardown(
-                () =>
+        /**
+         * @inheritdoc
+         */
+        protected ExecuteTests(): void
+        {
+            super.ExecuteTests();
+
+            test(
+                "Checking whether the tar-archive can be extracted…",
+                async () =>
                 {
-                    archive.Dispose();
-                    archiveDir.Dispose();
+                    await extract(
+                        {
+                            cwd: tempDir.FullName,
+                            file: this.Compiler.DestinationPath
+                        });
                 });
 
-            suite(
-                "Compile",
-                () =>
+            test(
+                "Checking whether the package-manifest exists inside the tar-archive…",
+                async () =>
                 {
-                    test(
-                        "Checking whether the instruction can be executed…",
-                        async () =>
+                    let files: string[] = [];
+
+                    await list(
                         {
-                            await compiler.Execute();
+                            file: this.Compiler.DestinationPath,
+                            onentry: (entry) =>
+                            {
+                                files.push(entry.header.path);
+                            }
                         });
 
-                    test(
-                        "Checking whether the tar-archive can be extracted…",
-                        async () =>
-                        {
-                            await extract(
-                                {
-                                    cwd: archiveDir.FullName,
-                                    file: archive.FullName
-                                });
-                        });
-
-                    test(
-                        "Checking whether the package-manifest exists inside the tar-archive…",
-                        async () =>
-                        {
-                            ok(await pathExists(archiveDir.MakePath("package.xml")));
-                        });
+                    ok(files.includes("package.xml"));
                 });
-        });
+        }
+    }("PackageCompiler").Register();
 }
