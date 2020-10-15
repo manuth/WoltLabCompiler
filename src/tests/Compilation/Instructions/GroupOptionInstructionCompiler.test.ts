824ddb7319b5dcf917c4075b88a6859ae65a67e3
Manuel Thalmann <m@nuth.ch>
2020-10-15 19:39:39 +0200

    Refactor the `GroupOptionInstructionCompiler` tests

--- src/tests/Compilation/Instructions/GroupOptionInstructionCompiler.test.ts
+++ src/tests/Compilation/Instructions/GroupOptionInstructionCompiler.test.ts
@@ -1,48 +1,35 @@
-import { ok } from "assert";
-import { TempDirectory } from "@manuth/temp-files";
-import { pathExists, readdir } from "fs-extra";
 import { GroupOptionInstructionCompiler } from "../../../Compilation/PackageSystem/Instructions/GroupOptionInstructionCompiler";
 import { ILocalization } from "../../../Globalization/ILocalization";
 import { GroupOptionInstruction } from "../../../PackageSystem/Instructions/Options/GroupOptionInstruction";
-import { Package } from "../../../PackageSystem/Package";
+import { CompilerTester } from "../TestComponents/Testers/CompilerTester";
+import { OptionInstructionCompilerTestRunner } from "../TestComponents/TestRunners/OptionInstructionCompilerTestRunner";
 
 /**
  * Registers tests for the `GroupOptionInstructionCompiler` class.
  */
 export function GroupOptionInstructionCompilerTests(): void
 {
-    suite(
-        "GroupOptionInstructionCompiler",
-        () =>
+    new class extends OptionInstructionCompilerTestRunner<CompilerTester<GroupOptionInstructionCompiler>, GroupOptionInstructionCompiler>
+    {
+        /**
+         * @inheritdoc
+         *
+         * @returns
+         * The new compiler-tester instance.
+         */
+        protected CreateTester(): CompilerTester<GroupOptionInstructionCompiler>
         {
-            let fileName: string;
-            let translationDir: string;
-            let tempDir: TempDirectory;
-            let compiler: GroupOptionInstructionCompiler;
-            let locales: string[];
+            let displayName: ILocalization = {};
+            let locales = ["en", "cn", "es"];
 
-            suiteSetup(
-                () =>
-                {
-                    let displayName: ILocalization = {};
-                    let $package: Package = new Package(
-                        {
-                            Identifier: "foo",
-                            DisplayName: {},
-                            InstallSet: {
-                                Instructions: []
-                            }
-                        });
+            for (let locale of locales)
+            {
+                displayName[locale] = "test";
+            }
 
-                    tempDir = new TempDirectory();
-                    locales = ["en", "cn", "es"];
-
-                    for (let locale of locales)
-                    {
-                        displayName[locale] = "test";
-                    }
-
-                    let instruction: GroupOptionInstruction = new GroupOptionInstruction(
+            return new CompilerTester(
+                new GroupOptionInstructionCompiler(
+                    new GroupOptionInstruction(
                         {
                             FileName: "options.xml",
                             Nodes: [
@@ -53,46 +40,7 @@ export function GroupOptionInstructionCompilerTests(): void
                                     }
                                 }
                             ]
-                        });
-
-                    $package.InstallSet.push(instruction);
-                    compiler = new GroupOptionInstructionCompiler(instruction);
-                    compiler.DestinationPath = tempDir.FullName;
-                    fileName = compiler.DestinationFileName;
-                    translationDir = tempDir.MakePath(instruction.DestinationRoot, instruction.TranslationDirectory);
-                });
-
-            suiteTeardown(
-                () =>
-                {
-                    tempDir.Dispose();
-                });
-
-            suite(
-                "Compile",
-                () =>
-                {
-                    test(
-                        "Checking whether the compiler can be executed…",
-                        async () =>
-                        {
-                            await compiler.Execute();
-                        });
-
-                    test(
-                        "Checking whether the option-file exists…",
-                        async () =>
-                        {
-                            ok(await pathExists(fileName));
-                        });
-
-                    test(
-                        "Checking whether the language-files exist…",
-                        async () =>
-                        {
-                            let files: string[] = await readdir(translationDir);
-                            ok(locales.every((locale: string) => files.includes(`${locale}.xml`)));
-                        });
-                });
-        });
+                        })));
+        }
+    }("GroupOptionInstructionCompiler").Register();
 }
