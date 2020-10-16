1c43ccfeca9bac978de2ae019c685b611f1d8dc3
Manuel Thalmann <m@nuth.ch>
2020-10-16 16:21:23 +0200

    Refactor the ThemeCompiler tests

--- src/tests/Compilation/Presentation/ThemeCompiler.test.ts
+++ src/tests/Compilation/Presentation/ThemeCompiler.test.ts
@@ -1,96 +1,90 @@
 import { ok } from "assert";
-import { TempDirectory, TempFile } from "@manuth/temp-files";
+import { TempFile } from "@manuth/temp-files";
 import { pathExists, writeJSON } from "fs-extra";
+import { join } from "upath";
 import { ThemeCompiler } from "../../../Compilation/Presentation/ThemeCompiler";
 import { ThemeInstruction } from "../../../PackageSystem/Instructions/Customization/Presentation/ThemeInstruction";
 import { Package } from "../../../PackageSystem/Package";
+import { CompilerTester } from "../TestComponents/Testers/CompilerTester";
+import { CompilerTestRunner } from "../TestComponents/TestRunners/CompilerTestRunner";
 
 /**
  * Registers tests for the `ThemeCompiler` class.
  */
 export function ThemeCompilerTests(): void
 {
-    suite(
-        "ThemeCompiler",
-        () =>
-        {
-            let tempDir: TempDirectory;
-            let variableFile: TempFile;
-            let variableFileName: string;
-            let compiler: ThemeCompiler;
+    let variableSource: TempFile;
 
-            suiteSetup(
+    new class extends CompilerTestRunner<CompilerTester<ThemeCompiler>, ThemeCompiler>
+    {
+        /**
+         * @inheritdoc
+         *
+         * @returns
+         * The new compiler-tester instance.
+         */
+        protected CreateTester(): CompilerTester<ThemeCompiler>
+        {
+            let variableFileName = "myVariableFile.xml";
+
+            let instruction: ThemeInstruction = new ThemeInstruction(
+                {
+                    Theme: {
+                        Name: "foo",
+                        DisplayName: {},
+                        VariableFileName: variableSource.FullName
+                    }
+                });
+
+            let extensionPackage: Package = new Package(
+                {
+                    Identifier: "bar",
+                    DisplayName: {},
+                    InstallSet: {
+                        Instructions: []
+                    }
+                });
+
+            extensionPackage.InstallSet.push(instruction);
+            return new CompilerTester(new ThemeCompiler(instruction.Theme, variableFileName));
+        }
+
+        /**
+         * @inheritdoc
+         */
+        protected async SuiteSetup(): Promise<void>
+        {
+            variableSource = new TempFile({ Suffix: ".json" });
+
+            await writeJSON(
+                variableSource.FullName,
+                {
+                    wcfHeaderBackground: "red"
+                });
+
+            await super.SuiteSetup();
+        }
+
+        /**
+         * @inheritdoc
+         */
+        protected ExecuteTests(): void
+        {
+            super.ExecuteTests();
+
+            test(
+                "Checking whether the theme-metadata exists…",
                 async () =>
                 {
-                    tempDir = new TempDirectory();
-
-                    variableFile = new TempFile(
-                        {
-                            Suffix: ".json"
-                        });
-
-                    variableFileName = "myVariableFile.xml";
-
-                    await writeJSON(
-                        variableFile.FullName,
-                        {
-                            wcfHeaderBackground: "red"
-                        });
-
-                    let instruction: ThemeInstruction = new ThemeInstruction(
-                        {
-                            Theme: {
-                                Name: "foo",
-                                DisplayName: {},
-                                VariableFileName: variableFile.FullName
-                            }
-                        });
-
-                    let extensionPackage: Package = new Package(
-                        {
-                            Identifier: "bar",
-                            DisplayName: {},
-                            InstallSet: {
-                                Instructions: []
-                            }
-                        });
-
-                    extensionPackage.InstallSet.push(instruction);
-                    compiler = new ThemeCompiler(instruction.Theme, variableFileName);
-                    compiler.DestinationPath = tempDir.FullName;
+                    ok(await pathExists(join(this.Compiler.DestinationPath, "style.xml")));
                 });
 
-            suiteTeardown(
-                () =>
+            test(
+                "Checking whether the variable-file exists…",
+                async () =>
                 {
-                    tempDir.Dispose();
-                    variableFile.Dispose();
+                    ok(await pathExists(join(this.Compiler.DestinationPath, this.Compiler.VariableFileName)));
                 });
-
-            suite(
-                "Compile",
-                () =>
-                {
-                    test(
-                        "Checking whether themes can be compiled without an error…",
-                        async () =>
-                        {
-                            await compiler.Execute();
-                        });
-
-                    test(
-                        "Checking whether the theme-metadata exists…",
-                        async () =>
-                        {
-                            ok(await pathExists(tempDir.MakePath("style.xml")));
-                        });
-
-                    test(
-                        "Checking whether the variable-file exists…",
-                        async () =>
-                        {
-                            ok(await pathExists(tempDir.MakePath(variableFileName)));
-                        });
-                });
-        });
+        }
+    }("ThemeCompiler").Register();
 }
